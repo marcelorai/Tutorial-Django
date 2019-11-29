07f27b7673aeccfbce2fc2e45f9dc60ead9f2e09
marcelorai <marcelorai1@hotmail.com>
2019-11-29 20:25:03 -0300

    Atualizando a view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -23,3 +25,7 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
